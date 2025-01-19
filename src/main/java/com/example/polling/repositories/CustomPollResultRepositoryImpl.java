c73fc1aaace0642cd161fb75c023528974205eb6
Grzegorz Płaczek <g01.placzek@gmail.com>
2025-01-19 20:03:47 +0100

    Use CassandraTemplate for vote count incrementation

--- src/main/java/com/example/polling/repositories/CustomPollResultRepositoryImpl.java
+++ src/main/java/com/example/polling/repositories/CustomPollResultRepositoryImpl.java
@@ -7,31 +7,26 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.cassandra.core.CassandraTemplate;
-import org.springframework.data.cassandra.core.cql.CqlTemplate;
 import org.springframework.data.cassandra.core.cql.QueryOptions;
 import org.springframework.data.cassandra.core.query.Criteria;
 import org.springframework.data.cassandra.core.query.Query;
+import org.springframework.data.cassandra.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 import com.datastax.oss.driver.api.core.ConsistencyLevel;
-import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 
 @Repository
 public class CustomPollResultRepositoryImpl implements CustomPollResultRepository {
     @Autowired
-    private CqlTemplate cqlTemplate;
-
-    @Autowired
     private CassandraTemplate cassandraTemplate;
 
-    public void incrementVoteCount(UUID uuid, int optionId) {
-        SimpleStatement statement = SimpleStatement.builder(
-            "UPDATE poll_results SET numofvotes  = numofvotes  + 1 WHERE poll_id = ? AND option_id = ?;")
-            .addPositionalValues(uuid, optionId)
-            .setConsistencyLevel(ConsistencyLevel.QUORUM)
-            .build();
-
-        this.cqlTemplate.execute(statement);
+    public void incrementVoteCount(UUID pollId, int optionId) {
+        Query query = Query.query(
+            Criteria.where("poll_id").is(pollId),
+            Criteria.where("option_id").is(optionId)
+        ).queryOptions(QueryOptions.builder().consistencyLevel(ConsistencyLevel.QUORUM).build());
+        Update update = Update.empty().increment("num_of_votes", 1);
+        this.cassandraTemplate.update(query, update, PollResult.class);
     }
 
     private List<PollResult> __findByKeyPollId(UUID pollId, ConsistencyLevel cl) {
